a8175046667a956ecef787521aa43a9b9202d726
howard <138744965+hank920217@users.noreply.github.com>
2026-06-11 10:47:43 +0800

    Update import from modle to model

--- tool2.py
+++ tool2.py
@@ -1,4 +1,4 @@
-from modle import FaceMeshDetector
+from model import FaceMeshDetector
 
 # 建立一個 FaceMeshDetector 物件
 detector = FaceMeshDetector()
@@ -6,6 +6,3 @@ detector = FaceMeshDetector()
 # 使用 detect 方法來處理一張圖片
 for i in  range(1, 201):
     detector.detect(r"C:\Users\stone\MediaPipe\data\sad\sad({}).jpg".format(i))
-
-
-
